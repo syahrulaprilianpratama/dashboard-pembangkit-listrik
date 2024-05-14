bb548431461d51a72cde7ed666e0d8460c7bb09c
syahrulaprilianpratama <syahrul.23020@mhs.unesa.ac.id>
2024-05-14 20:56:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ st.sidebar.title("Data Science for Energy System Modelling")
 st.sidebar.markdown(
     ":+1: This notebook introduces you to the streamlit library.")
 
-hover_data = ['name', 'primary_fuel', "capacity_mw", 'owner']
+hover_data = ['name', 'primary_fuel', "capacity_mw", 'owner','commingssioning
+             -year']
 
 if not df.empty:
     fig = px.scatter_mapbox(
